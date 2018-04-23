88b5c4c3d7f259b0853c50bb316339b0e13ce24c
Equintana89 <34198423+Equintana89@users.noreply.github.com>
2018-04-23 16:40:55 -0600

    Add files via upload

---
+++ Web Dev Projects/Eloquentjs.js
@@ -0,0 +1,49 @@
+//LOOPING A TRIANGLE
+
+/*let x="#";
+  while(x<="#######"){
+    console.log(x);
+    x = x + "#";
+}*/
+
+
+
+//FIZZBUZZ
+
+/*for(let x=0; x<=100; x++){
+	if(x % 3){
+	console.log("Fizz");
+	}else{
+		if(x % 5){
+			console.log("Buzz");
+		}else{
+			console.log("FizzBuzz");
+		}
+	}
+
+}*/
+
+
+
+
+//CHESS BOARD
+
+
+
+let x="";
+let size=8;
+
+for(counter1 = 0; counter1 < size; counter1++){
+	for(counter2 = 0; counter2 < size; counter2++){
+		if((counter1 + counter2) % 2 == 0) {
+			x += "#";
+		}else{
+			x += " ";
+		}
+	}
+	x += "\n";
+}
+console.log(x);
+
+
+
