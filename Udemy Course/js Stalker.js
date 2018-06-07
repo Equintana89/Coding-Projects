2817d2d54afc70d1fd958c672333ba5251ae8c4c
Equintana89 <34198423+Equintana89@users.noreply.github.com>
2018-06-07 08:01:42 -0600

    Delete js Stalker.js

--- Udemy Course/js Stalker.js
+++
@@ -1,9 +0,0 @@
-let firstName = prompt("What is your first name?");
-let lastName = prompt("What is your last name?");
-let age = prompt("How old are you?");
-let fullName = firstName + " "+ lastName;
-console.log("Your name is " + fullName);
-console.log("You are " + age + " years old.");
-
-// alert("Nice to meet you " + userName);
-// console.log("Nice to meet you too " + userName);
