c6c963a4d69a7c56f2fec1ab3312cd11b9362326
Eduardo Quintana <3dd1389@gmail.com>
2018-07-02 20:20:46 -0600

    Completed game

--- Udemy-JavaScript/game.js
+++ Udemy-JavaScript/game.js
@@ -17,8 +17,8 @@ init();
 document.querySelector('.btn-roll').addEventListener('click', function() {
     if(gamePlaying) {
         // 1. Random number
-        var dice1 = Math.floor(Math.random() * 6) + 1;
-        var dice2 = Math.floor(Math.random() * 6) + 1;
+        let dice1 = Math.floor(Math.random() * 6) + 1;
+        let dice2 = Math.floor(Math.random() * 6) + 1;
 
 
         //2. Display the result
