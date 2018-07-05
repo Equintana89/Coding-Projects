3673ce43c5e5804f9e366330b84060b5974b334b
Eduardo Quintana <3dd1389@gmail.com>
2018-07-05 17:22:38 -0600

    Update file

--- Udemy-JavaScript/advancedjs.js
+++ Udemy-JavaScript/advancedjs.js
@@ -74,7 +74,7 @@ console.log(okc.league);*/
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 //PRACTICE
 
-var tesla = {
+/*var tesla = {
 	model: 'Model S',
 	price: 35000,
 	taxDeduction: 7500
@@ -103,7 +103,103 @@ toyota.discount();
 
 console.log(tesla.carType);
 console.log(ford.carType);
-console.log(toyota.carType);
+console.log(toyota.carType);*/
+
+
+
+
+//Object.create
+
+/*let personProto = {
+	calculateAge: function(){
+		console.log(2016 - this.yearOfBirth);
+	}
+}
+
+let john = Object.create(personProto);
+john.name = 'John';
+john.yearOfBirth = '1990';
+john.job = 'teacher';
+
+let jane = Object.create(personProto, {
+	name: { value: 'Jane' },
+	yearOfBirth: { value: 1969 },
+	job: { value: 'designer' }
+});*/
+	
+
+
+
+// Primitives vs objects
+
+//Primitives
+let a = 23;
+let b = a;
+a = 46; //Mutated/changed variable
+console.log(a);
+console.log(b);
+
+
+
+//Objects
+let obj1 = {
+	name: 'John',
+	age: 26
+};
+
+let obj2 = obj1;
+obj1.age = 30;
+console.log(obj1.age);
+console.log(obj2.age);
+
+//Functions
+let age = 27;
+let obj = {
+	name: 'Jonas',
+	city: 'Lisbon'
+};
+
+function change(a, b) {
+	a = 30;
+	b.city = 'San Francisco';
+}
+
+change(age, obj);
+console.log(age);
+console.log(obj.city);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
