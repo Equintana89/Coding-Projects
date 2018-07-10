3d13547a58353c8281422d75594cdf99c17ce7cc
Eduardo Quintana <3dd1389@gmail.com>
2018-07-09 23:23:21 -0600

    Coding challenge

--- Udemy-JavaScript/advancedjs.js
+++ Udemy-JavaScript/advancedjs.js
@@ -442,10 +442,68 @@ let fullJapan = arrayCalc(ages, isFullAge.bind(this, 20));
 console.log(ages);
 console.log(fullJapan);*/
 
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Coding challenge
+
+/*
+--- Let's build a fun quiz game in the console! ---
+1. Build a function constructor called Question to describe a question. A question should include:
+a) question itself
+b) the answers from which the player can choose the correct one (choose an adequate data structure here, array, object, etc.)
+c) correct answer (I would use a number for this)
+2. Create a couple of questions using the constructor
+3. Store them all inside an array
+4. Select one random question and log it on the console, together with the possible answers (each question should have a number) (Hint: write a method for the Question objects for this task).
+5. Use the 'prompt' function to ask the user for the correct answer. The user should input the number of the correct answer such as you displayed it on Task 4.
+6. Check if the answer is correct and print to the console whether the answer is correct ot nor (Hint: write another method for this).
+7. Suppose this code would be a plugin for other programmers to use in their code. So make sure that all your code is private and doesn't interfere with the other programmers code (Hint: we learned a special technique to do exactly that).
+*/
+
+ 
 
 
 
+(function(){
+function Question(question, answer, correct) {
+	this.question = question;
+	this.answer = answer;
+	this.correct = correct;	
+}
+
+Question.prototype.displayQuestion = function() {
+	console.log(this.question)
+
+	for(let i = 0; i < this.answer.length; i++){
+		console.log(i + ': ' + this.answers[i]);
+	}
+}
+
+Question.prototype.checkAnswer = function () {
+	if(ans === this.answer){
+		console.log('Correct!');
+	
+
+} else {
+	console.log('Wrong answer! Try again');
+}
+
+let q1 = new Question('Who won the 1997 NBA finals?', ['Chicago Bulls', 'Cleveland Caveliers', 'Utah Jazz'], 1);
+
+let q2 = new Question('Who won the 2018 NBA finals?', ['Cleveland  Caveliers', 'Houston Rockets', 'Golden State Warriors'], 2);
+
+let q3 = new Question('Who won the 2016 NBA finals MVP?', ['Lebron James', 'Kyrie Irving', 'Kevin Love'], 3);
+
+let questions = [q1, q2, q3];
+
+let n = Math.floor(Math.random() * questions.length);
+
+question[n].displayQuestion();
+
+var answer = parseInt(prompt('Please select the correct answer.'));
+
+questions[n].checkAnswer(answer);
 
 
-
-
+  }
+	
+})();
