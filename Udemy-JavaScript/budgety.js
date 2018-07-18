db4bfe896d66088b4a13eded3cd1a66eab03414c
Eduardo Quintana <3dd1389@gmail.com>
2018-07-18 09:49:55 -0600

    Structuring project

---
+++ Udemy-JavaScript/budgety.js
@@ -0,0 +1,75 @@
+
+//BUDGET CONTROLLER
+let budgetController = (function() {
+
+
+})();
+
+
+
+//UI CONTROLLER
+let UIController = (function() {
+
+	//some code
+
+})();
+
+
+
+
+//GLOBAL APP CONTROLLER
+let controller = (function(budgetCtrl, UICtrl) {
+
+	let ctrlAddItem = function() {
+
+		// 1. Get the field input data
+
+		// 2. Add the item to the budget controller
+
+		// 3. Add the item to the UI
+
+		// 4. Calculate the budget
+
+		// 5. Display the budget on the UI
+
+		console.log('It works')
+
+	}
+
+	document.querySelector('.add__btn').addEventListener('click', ctrlAddItem);
+
+
+	document.addEventListener('keypress', function(event) {
+
+		if(event === 13 || event.which === 13) {
+			ctrlAddItem();
+
+
+		}
+
+	});
+
+})(budgetController, UIController); 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
