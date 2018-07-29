8bfa78a64059aa5e6c074dafab067b9045dc5955
Eduardo Quintana <3dd1389@gmail.com>
2018-07-29 00:22:40 -0600

    password validation

--- Web Dev Projects/SnoShredd/Snoform.js
+++ Web Dev Projects/SnoShredd/Snoform.js
@@ -1,5 +1,10 @@
 let email = document.querySelector('#inputEmail');
 let password = document.querySelector('#inputPassword');
+let letter = document.querySelector('#letter');
+let capital = document.querySelector('#capital');
+let number = document.querySelector('#number');
+let length = document.querySelector('#length');
+
 
 inputEmail.addEventListener('input', function(event) {
 	if(inputEmail.validity.typeMismatch) {
@@ -9,10 +14,50 @@ inputEmail.addEventListener('input', function(event) {
 	}
 });
 
-inputPassword.addEventListener('input', function(event) {
-	if (inputPassword.validity.typeMismatch) {
-		inputPassword.setCustomValidity('Password requirements are being used.');
+
+password.onFocus = function() {
+	document.querySelector('#message').style.display = 'block';
+}
+
+password.onBlur = function() {
+	document.querySelector('#message').style.display = 'none';
+}
+
+password.onKeyup = function() {
+	let lowerCaseLetters = /[a-z]/g;
+		if(password.value.match(lowerCaseLetters)) {
+			letter.classList.remove('invalid');
+			letter.classList.add('valid');
+		} else {
+			letter.classList.remove('valid');
+			letter.classList.add('invalid')
+		}
+}
+
+let upperCaseLetters = /[A-Z]/g;
+	if(password.value.match(upperCaseLetters)) {
+		capital.classList.remove('invalid');
+		capital.classList.add('valid');
 	} else {
-		inputPassword.setCustomValidity('');
+		capital.classList.remove('valid');
+		capital.classList.add('invalid');
 	}
-});
+
+let numbers = /[0-9]/g;
+	if(password.value.match(numbers)) {
+		number.classList.remove('invalid');
+		number.classList.add('valid');
+	} else {
+		number.classList.remove('valid');
+		number.classList.add('invalid');
+	}
+
+	if(password.value.length >=8) {
+		length.classList.remove('invalid');
+		length.classList.add('valid');
+	} else {
+		length.classList.remove('valid');
+		length.classList.add('invalid');
+	}
+
+
