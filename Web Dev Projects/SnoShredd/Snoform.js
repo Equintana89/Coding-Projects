3ed487c0b0b534c463808c6338d7f48d0574c343
Eduardo Quintana <3dd1389@gmail.com>
2018-08-01 00:39:30 -0600

    Commented out majority of snoform.js

--- Web Dev Projects/SnoShredd/Snoform.js
+++ Web Dev Projects/SnoShredd/Snoform.js
@@ -20,45 +20,45 @@ inputEmail.addEventListener('input', function(event) {
 
 
 //When the user starts to type something inside the password field
-password.onkeyup = function() {
-	//validate lowercase letters
-	let lowerCaseLetters = /[a-z]/g;
-		if(password.value.match(lowerCaseLetters)) {
-			letter.classList.remove('invalid');
-			letter.classList.add('valid');
-		} else {
-			letter.classList.remove('valid');
-			letter.classList.add('invalid')
-		}
-};
+// password.onkeyup = function() {
+// 	//validate lowercase letters
+// 	let lowerCaseLetters = /[a-z]/g;
+// 		if(password.value.match(lowerCaseLetters)) {
+// 			letter.classList.remove('invalid');
+// 			letter.classList.add('valid');
+// 		} else {
+// 			letter.classList.remove('valid');
+// 			letter.classList.add('invalid')
+// 		}
+// };
 
-//validate capital letters
-let upperCaseLetters = /[A-Z]/g;
-	if(password.value.match(upperCaseLetters)) {
-		capital.classList.remove('invalid');
-		capital.classList.add('valid');
-	} else {
-		capital.classList.remove('valid');
-		capital.classList.add('invalid');
-	}
+// //validate capital letters
+// let upperCaseLetters = /[A-Z]/g;
+// 	if(password.value.match(upperCaseLetters)) {
+// 		capital.classList.remove('invalid');
+// 		capital.classList.add('valid');
+// 	} else {
+// 		capital.classList.remove('valid');
+// 		capital.classList.add('invalid');
+// 	}
 
-//validate numbers
-let numbers = /[0-9]/g;
-	if(password.value.match(numbers)) {
-		number.classList.remove('invalid');
-		number.classList.add('valid');
-	} else {
-		number.classList.remove('valid');
-		number.classList.add('invalid');
-	}
+// //validate numbers
+// let numbers = /[0-9]/g;
+// 	if(password.value.match(numbers)) {
+// 		number.classList.remove('invalid');
+// 		number.classList.add('valid');
+// 	} else {
+// 		number.classList.remove('valid');
+// 		number.classList.add('invalid');
+// 	}
 
-	//validate length
-	if(password.value.length >= 8) {
-		length.classList.remove('invalid');
-		length.classList.add('valid');
-	} else {
-		length.classList.remove('valid');
-		length.classList.add('invalid');
-	}
+// 	//validate length
+// 	if(password.value.length >= 8) {
+// 		length.classList.remove('invalid');
+// 		length.classList.add('valid');
+// 	} else {
+// 		length.classList.remove('valid');
+// 		length.classList.add('invalid');
+// 	}
 
 
