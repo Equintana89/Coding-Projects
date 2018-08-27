582154e9fbf277577c02ad13cfa7bf33f7e9909a
Eduardo Quintana <3dd1389@gmail.com>
2018-08-27 16:52:28 -0600

    displaying list, adding click event handler

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -9,10 +9,16 @@ import {HEROES} from '../mock-heroes/mock-heroes.component';
 })
 export class HeroesComponent implements OnInit {
   heroes = HEROES;
- 
+
+  selectedHero: Hero;
+  
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
   constructor() { }
 
   ngOnInit() {
   }
+  
 }
 
