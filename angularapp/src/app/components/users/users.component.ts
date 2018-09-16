53413fb627e7dcb4410293d2ff738bf60265feef
Eduardo Quintana <3dd1389@gmail.com>
2018-09-15 23:15:35 -0600

    Keyboard and input events, added Add User form

--- angularapp/src/app/components/users/users.component.ts
+++ angularapp/src/app/components/users/users.component.ts
@@ -11,6 +11,7 @@ export class UsersComponent implements OnInit {
     showExtended: boolean = true;
     loaded: boolean = false;
     enableAdd: boolean = true;
+    showUserForm: boolean = false;
     
 
   constructor() { }
@@ -112,6 +113,18 @@ export class UsersComponent implements OnInit {
       this.users.push(user);
     }
 
+    onSubmit(e) {
+      console.log(123);
+
+      e.preventDefault();
+    }
+
+    fireEvent(e) {
+      console.log(e.type);
+      console.log(e.target.value);
+      
+
+    }
   
   //  toggleHide(user) {
   //    user.hide = !user.hide; //this line can be added to the html file instead.
