c5ec5b42cd785a799f35509df6413f47e3e23878
Eduardo Quintana <3dd1389@gmail.com>
2018-10-04 23:01:50 -0600

    Add, update and delete logs

--- devlogger/src/app/components/log-form/log-form.component.ts
+++ devlogger/src/app/components/log-form/log-form.component.ts
@@ -10,10 +10,12 @@ import { Log } from '../../models/log';
   styleUrls: ['./log-form.component.css']
 })
 export class LogFormComponent implements OnInit {
+  logs: Log[];
   id: string;
   text: string;
   date: any;
 
+  isNew: boolean = true;
 
   constructor(private logService: LogService) { }
 
@@ -22,10 +24,40 @@ export class LogFormComponent implements OnInit {
     this.logService.selectedLog.subscribe(log => {
       if(log.id !== null)  {
         this.id = log.id;
+        this.isNew = false;
         this.text = log.text;
         this.date = log.date;
       }
     });
   }
+  onSubmit() {
+    //Check id new log
+    if(this.isNew) {
+      //create new log
+      const newLog = {
+        id: this.generateId(),
+        text: this.text,
+        date: new Date()
+      }
+      //Add log
+      this.logService.addLog(newLog);
+    } else {
+      //Create log to be updated
+      const updLog = {
+        id: this.id,
+        text: this.text,
+        date: new Date()
+      }
+      //Update Log
+      this.logService.updateLog(updLog);
+    }
+  }
 
-}
+  generateId() {
+      return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+        var r = Math.random() * 16 | 0, v = c == 'x' ? r : (r & 0x3 | 0x8);
+        return v.toString(16);
+      });
+    }
+  }
+
