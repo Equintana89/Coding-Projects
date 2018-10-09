00a791df79ce93c99f0f70ebae65793decd858ec
Eduardo Quintana <3dd1389@gmail.com>
2018-10-08 21:21:09 -0600

    Finishing up the state and UX

--- devlogger/src/app/services/log.service.ts
+++ devlogger/src/app/services/log.service.ts
@@ -19,11 +19,13 @@ export class LogService {
   stateClear = this.stateSource.asObservable();
   
   constructor() {
-    this.logs = [
-      {id: '1', text: 'Generated components', date: new Date('12/26/2017 12:54:53')},
-      {id: '2', text: 'Added Bootstrap', date: new Date('12/27/2017 2:54:53')},
-      {id: '1', text: 'Added logs component', date: new Date('12/26/2017 4:23:53')}
-    ]
+
+    this.logs = [];
+    // this.logs = [
+    //   {id: '1', text: 'Generated components', date: new Date('12/26/2017 12:54:53')},
+    //   {id: '2', text: 'Added Bootstrap', date: new Date('12/27/2017 2:54:53')},
+    //   {id: '3', text: 'Added logs component', date: new Date('12/28/2017 4:23:53')}
+    // ]
    }
 
    getLogs(): Observable<Log[]> {
