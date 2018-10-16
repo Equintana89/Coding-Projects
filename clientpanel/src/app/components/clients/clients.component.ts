7397807664c95b3cf60ce831438ab5c325445834
Eduardo Quintana <3dd1389@gmail.com>
2018-10-16 07:46:32 -0600

    Working on adding client form and validations

--- clientpanel/src/app/components/clients/clients.component.ts
+++ clientpanel/src/app/components/clients/clients.component.ts
@@ -16,13 +16,15 @@ export class ClientsComponent implements OnInit {
 
   ngOnInit() {
     this.clientService.getClients().subscribe(clients => {
-      this.clients = clients});
+      this.clients = clients
       this.getTotalOwed();
+    });
+      
   }
 
   getTotalOwed() {
     this.totalOwed = this.clients.reduce((total, client) => {
-      return total + client.balance;
+      return total += client.balance;
     }, 0);
 
 
