379a9e360b2f60c61b8f245bb8a3fe3152539a26
Eduardo Quintana <eduardoq@dentalselect.com>
2019-08-15 23:17:48 -0600

    Initial commit

--- snowboards/src/app/app.component.ts
+++ snowboards/src/app/app.component.ts
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'snowboards';
+  title = 'Snowboards';
+  phone = '801-867-5653'
 }
+
+
